5ef9876418297ac2b309d98af28e258d12044bb3
Rafael <rafaelgorayb388@gmail.com>
2024-07-10 14:57:52 -0300

    alterações card carro

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,31 +8,28 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ car }) => {
     return (
-        <div className="car-card group p-2 border rounded-lg h-85">
-          <div className="relative w-full h-42  mb-4">
-            <Image src="/carroTeste.png" alt="Carro" layout="fill" objectFit="cover" className="rounded-lg" />
-          </div>
-          <div className="car-card__content text-leading">
-            <h2 className="text-md font-bold">
-              {car.Marca} {car.Modelo}
-            </h2>
-            <h3 className="text-sm font-semibold text-red-500">{car.Versao}</h3>
-          </div>
-          <div className="car-card__details columns-2 text-sm mt-2">
-            <div>
-            <p className="">Ano</p>
-            <p className="">{car.Especificacoes.ano_de_fabricacao}/{car.Especificacoes.ano_do_modelo}</p>
-            </div>
-           <div>
-           <p className="">Km</p>
-            <p className="">{car.Especificacoes.km.toLocaleString('pt-BR')}</p>
-           </div>
-
-          </div>
-          <p className='mt-2 text-xl font-semibold '>
-            R${car.Preco.toLocaleString('pt-BR')}
-          </p>
-        </div>
+<div className="w-44 h-80 bg-white rounded-xl shadow flex-col justify-start items-center gap-2.5 inline-flex">
+  <img className="self-stretch h-44 rounded-tl-xl rounded-tr-xl" src="/carroTeste.png" />
+  <div className="self-stretch h-8 px-3 flex-col justify-center items-start flex">
+    <div className="text-black text-xs font-medium leading-none">{car.Marca} {car.Modelo}  </div>
+    <div className="justify-center items-center gap-2.5 inline-flex">
+      <div className="text-red-600 text-xs font-medium leading-none">{car.Versao}</div>
+    </div>
+  </div>
+  <div className="self-stretch px-3 bg-white/opacity-5 rounded-lg justify-start items-start inline-flex">
+    <div className="w-20 flex-col justify-start items-start gap-0.5 inline-flex">
+      <div className="self-stretch text-neutral-400 text-xs font-normal leading-none">Ano</div>
+      <div className="w-20 h-4 text-zinc-900 text-xs font-medium leading-none">{car.Especificacoes.ano_de_fabricacao}/{car.Especificacoes.ano_do_modelo}</div>
+    </div>
+    <div className="w-20 flex-col justify-start items-start gap-0.5 inline-flex">
+      <div className="self-stretch text-neutral-400 text-xs font-normal leading-none">Km</div>
+      <div className="self-stretch text-zinc-900 text-xs font-medium leading-none">{car.Especificacoes.km}</div>
+    </div>
+  </div>
+  <div className="self-stretch h-4 px-3 bg-white/opacity-5 rounded-lg flex-col justify-center items-start flex">
+    <div className="w-36 h-4 text-black text-sm font-semibold leading-none">R$ {car.Preco}</div>
+  </div>
+</div>
     );
 };
 
