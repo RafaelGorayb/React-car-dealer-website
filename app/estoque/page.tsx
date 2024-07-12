9cc399897a70292bbda5f725fa8ac8f018c8c8cc
Rafael <rafaelgorayb388@gmail.com>
2024-07-12 12:03:43 -0300

    adição do skeleton

--- app/estoque/page.tsx
+++ app/estoque/page.tsx
@@ -1,3 +1,4 @@
+// Estoque.tsx
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -14,20 +15,21 @@ import { useCompareList } from "@/lib/userState";
 import CarCard from "@/components/Card";
 import CarFilter from "@/components/carFilter";
 import { FilterSchemaType } from "@/lib/formTypes";
-import { Spinner } from "@nextui-org/react";
 
 function Estoque() {
   const [cars, setCars] = useState<Car[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { compareList, setCompareList } = useCompareList();
 
   useEffect(() => {
     const fetchCars = async () => {
       const carsCollection = collection(db, "carros");
-      const carsQuery = query(carsCollection, limit(4));
+      const carsQuery = query(carsCollection, limit(8));
       const carsSnapshot = await getDocs(carsQuery);
       const carsList = carsSnapshot.docs.map((doc) => doc.data() as Car);
       setCars(carsList);
+      setIsLoading(false);
     };
 
     fetchCars();
@@ -60,17 +62,21 @@ function Estoque() {
 
   return (
     <div className="flex flex-col md:flex-row h-screen ">
-      <div className="w-full md:w-1/4 p-4">
+      <div className="w-full md:w-1/4 mb-4">
         <h2 className="text-2xl font-bold mb-4">Filtros</h2>
         <CarFilter submitForm={handleFilterSubmit} />
       </div>
 
       <div className="w-full md:w-3/4 overflow-y-auto">
-        <div className="grid justify-items-center grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 lg:gap-8 gap-4">
-          {cars.length === 0 ? (
-            <Spinner className="justify-items-center"/>
+        <div className="grid gap-4 justify-items-center grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
+          {cars.length === 0 && isLoading ? (
+            Array.from({ length: 8 }).map((_, index) => (
+              <CarCard key={index} car={{} as Car} isLoading={true} />
+            ))
           ) : (
-            cars.map((car, index) => <CarCard key={index} car={car} />)
+            cars.map((car, index) => (
+              <CarCard key={index} car={car} isLoading={false} />
+            ))
           )}
         </div>
       </div>
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,19 +1,65 @@
 "use client";
 import React from "react";
 import { Car } from "../types";
-import { Card, CardBody, CardFooter, Button } from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Skeleton } from "@nextui-org/react";
 import { useCompareList } from "@/lib/userState";
 import '../styles/globals.css';
-import { Fullscreen } from "lucide-react";
 
 interface CardProps {
   car: Car;
+  isLoading: boolean;
 }
-
-function CarCard({ car }: CardProps) {
+//
+function CarCard({ car, isLoading }: CardProps) {
   function handleAddToCompare() {
     const { setCompareList } = useCompareList();
     setCompareList((prev) => [...(prev || []), car]);
+  }
+
+  if (isLoading) {
+    return (
+      <Card className="md:w-lg md:h-lg w-sm shadow-2xl" radius="sm" isPressable={true} isHoverable={true}>
+        <div className="relative w-full h-[170px]">
+          <Skeleton isLoaded={!isLoading} className="w-full h-full object-cover">
+            <div className="w-full h-full bg-gray-200"></div>
+          </Skeleton>
+        </div>
+
+        <CardBody className="overflow-visible py-2">
+          
+            <div>
+            <Skeleton className= "rounded-lg h-4 w-3/4" isLoaded={!isLoading}>
+              <p className="text-xs font-semibold bg-gray-200 h-4 w-3/4"></p>
+            </Skeleton>
+            <Skeleton className= "rounded-lg h-4 w-2/4 mt-1" isLoaded={!isLoading}>
+              <h3 className="text-sm font-semibold text-red-500 bg-gray-200 h-4 w-1/2 mt-2"></h3>
+            </Skeleton>            
+              <div className="flex text-sm flex-row gap-4 py-2">
+              <Skeleton className= "rounded-lg w-2/4 mt-1" isLoaded={!isLoading}>
+                <div className="flex flex-col">
+                  <p className="text-neutral-400 text-xs">Ano</p>
+                  <p className="font-medium  text-sm bg-gray-200 h-4 w-1/2"></p>
+                </div>
+                </Skeleton>
+                <Skeleton className= "rounded-lg w-2/4 mt-1" isLoaded={!isLoading}>
+                <div className="flex flex-col">
+                  <p className="text-neutral-400 text-xs">Km</p>
+                  <p className="font-medium text-sm bg-gray-200 h-4 w-1/2"></p>
+                </div>
+                </Skeleton>
+              </div>
+            
+              
+            </div>
+         
+        </CardBody>
+        <CardFooter className="">
+          <Skeleton className="rounded-lg h-4 w-3/4" isLoaded={!isLoading}>
+            <p className="text-md font-semibold bg-gray-200 h-4 w-1/3"></p>
+          </Skeleton>
+        </CardFooter>
+      </Card>
+    );
   }
 
   return (
@@ -35,29 +81,29 @@ function CarCard({ car }: CardProps) {
           <div className="flex text-sm flex-row gap-4 py-2">
             <div className="flex flex-col">
               <p className="text-neutral-400 text-xs">Ano</p>
-              <p className="font-medium text-stone-900 text-sm">
-                {car.Especificacoes.ano_de_fabricacao}/
-                {car.Especificacoes.ano_do_modelo}
+              <p className="font-medium  text-sm">
+                {car.Especificacoes?.ano_de_fabricacao}/
+                {car.Especificacoes?.ano_do_modelo}
               </p>
             </div>
             <div className="flex flex-col">
               <p className="text-neutral-400 text-xs">Km</p>
-              <p className="font-medium text-stone-900 text-sm">
-                {car.Especificacoes.km.toLocaleString("pt-BR")}
+              <p className="font-medium text-sm">
+                {car.Especificacoes?.km?.toLocaleString("pt-BR")}
               </p>
             </div>
           </div>
-          <p className="mt-2 text-md font-semibold">
-            R${car.Preco.toLocaleString("pt-BR")}
-          </p>
         </div>
       </CardBody>
-      {/* <CardFooter className="flex justify-between gap-4 px-4">
-        <Button onPress={handleAddToCompare} color="secondary">
+      <CardFooter className="">
+        <p className="text-md font-semibold">
+          R${car.Preco?.toLocaleString("pt-BR")}
+        </p>
+        {/* <Button onPress={handleAddToCompare} color="secondary">
           Adicionar ao comparador
         </Button>
-        <Button color="primary">Ver mais</Button>
-      </CardFooter> */}
+        <Button color="primary">Ver mais</Button> */}
+      </CardFooter>
     </Card>
   );
 }
