409b99e70698356cb91dee1e85574dcae102b639
Rafael <rafaelgorayb388@gmail.com>
2024-07-31 11:47:22 -0300

    arrumar layout do card

--- components/Card.tsx
+++ components/Card.tsx
@@ -36,90 +36,104 @@ function CarCard({ car, isLoading }: CardProps) {
 
   const renderCardContent = () => (
     <>
-      <div className="relative w-full h-[200px] md:h-[250px]">
-        <img
-          src={car.fotos[0] || "/carroTeste.png"}
-          className="w-full h-full object-cover"
-          alt={`${car.marca} ${car.modelo}`}
-        />
-      </div>
+    <div className="relative w-full h-[170px]">
+      <img
+        src={car.fotos[0] || "/carroTeste.png"}
+        className="w-full h-full object-cover"
+        alt={`${car.marca} ${car.modelo}`}
+      />
+    </div>
 
-      <CardBody className="overflow-visible py-2">
-        <div className="flex flex-col md:flex-row md:justify-between">
-          <div>
-            <p className="text-sm md:text-base font-semibold">
-              {car.marca} {car.modelo}
+    <CardBody className="overflow-visible py-2">
+      <div>
+        <p className="text-xs font-semibold">
+          {car.marca} {car.modelo}
+        </p>
+        <h3 className="text-sm font-semibold text-red-500 min-h-10 line-clamp-2">
+          {car.versao}
+        </h3>
+        <div>
+          {car.blindado && (
+            <Chip
+              startContent={<Shield size={12} />}
+              variant="solid"
+              color="danger"
+              size="sm"
+            >
+              Blindado
+            </Chip>
+          )}
+        </div>
+        <div className="flex text-sm flex-row gap-4 py-2">
+          <div className="flex flex-col">
+            <p className="text-neutral-400 text-xs">Ano</p>
+            <p className="font-medium text-sm">
+              {car.ano_fabricacao}/{car.ano_modelo}
             </p>
-            <h3 className="text-sm md:text-lg font-semibold text-red-500 min-h-10 line-clamp-2">
-              {car.versao}
-            </h3>
-            <div className="mt-2">
-              {car.blindado && (
-                <Chip
-                  startContent={<Shield size={12} />}
-                  variant="solid"
-                  color="danger"
-                  size="sm"
-                >
-                  Blindado
-                </Chip>
-              )}
-            </div>
           </div>
-          <div className="flex flex-row md:flex-col gap-4 py-2 md:py-0 md:text-right">
-            <div className="flex flex-col">
-              <p className="text-neutral-400 text-xs">Ano</p>
-              <p className="font-medium text-sm md:text-base">
-                {car.ano_fabricacao}/{car.ano_modelo}
-              </p>
-            </div>
-            <div className="flex flex-col">
-              <p className="text-neutral-400 text-xs">Km</p>
-              <p className="font-medium text-sm md:text-base">
-                {car.km.toLocaleString("pt-BR")}
-              </p>
-            </div>
+          <div className="flex flex-col">
+            <p className="text-neutral-400 text-xs">Km</p>
+            <p className="font-medium text-sm">
+              {car.km.toLocaleString("pt-BR")}
+            </p>
           </div>
         </div>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <p className="text-lg md:text-xl font-semibold">
-          R$ {car.preco.toLocaleString("pt-BR")}
-        </p>
-      </CardFooter>
-    </>
+      </div>
+    </CardBody>
+    <CardFooter className="">
+      <p className="text-md font-semibold">
+        R$ {car.preco.toLocaleString("pt-BR")}
+      </p>
+    </CardFooter>
+  </>
   );
 
   if (isLoading) {
     return (
       <Card
-        className="w-full md:max-w-[400px] shadow-lg"
+        className="md:w-lg md:h-lg w-sm shadow-2xl"
         radius="sm"
         isPressable={true}
         isHoverable={true}
       >
-        <Skeleton className="rounded-lg">
-          <div className="h-[200px] md:h-[250px]"></div>
-        </Skeleton>
+        <div className="relative w-full h-[170px]">
+          <Skeleton
+            isLoaded={!isLoading}
+            className="w-full h-full object-cover"
+          >
+            <div className="w-full h-full bg-gray-200"></div>
+          </Skeleton>
+        </div>
         <CardBody className="overflow-visible py-2">
-          <Skeleton className="w-3/4 rounded-lg">
-            <div className="h-4 md:h-5"></div>
-          </Skeleton>
-          <Skeleton className="w-1/2 mt-2 rounded-lg">
-            <div className="h-4 md:h-5"></div>
-          </Skeleton>
-          <div className="flex justify-between mt-4">
-            <Skeleton className="w-1/4 rounded-lg">
-              <div className="h-4"></div>
+          <div>
+            <Skeleton className="rounded-lg h-4 w-3/4" isLoaded={!isLoading}>
+              <p className="text-xs font-semibold bg-gray-200 h-4 w-3/4"></p>
             </Skeleton>
-            <Skeleton className="w-1/4 rounded-lg">
-              <div className="h-4"></div>
+            <Skeleton
+              className="rounded-lg h-4 w-2/4 mt-1"
+              isLoaded={!isLoading}
+            >
+              <h3 className="text-sm font-semibold text-red-500 bg-gray-200 h-4 w-1/2 mt-2"></h3>
             </Skeleton>
+            <div className="flex text-sm flex-row gap-4 py-2">
+              <Skeleton className="rounded-lg w-2/4 mt-1" isLoaded={!isLoading}>
+                <div className="flex flex-col">
+                  <p className="text-neutral-400 text-xs">Ano</p>
+                  <p className="font-medium text-sm bg-gray-200 h-4 w-1/2"></p>
+                </div>
+              </Skeleton>
+              <Skeleton className="rounded-lg w-2/4 mt-1" isLoaded={!isLoading}>
+                <div className="flex flex-col">
+                  <p className="text-neutral-400 text-xs">Km</p>
+                  <p className="font-medium text-sm bg-gray-200 h-4 w-1/2"></p>
+                </div>
+              </Skeleton>
+            </div>
           </div>
         </CardBody>
-        <CardFooter>
-          <Skeleton className="w-1/3 rounded-lg">
-            <div className="h-6"></div>
+        <CardFooter className="">
+          <Skeleton className="rounded-lg h-4 w-3/4" isLoaded={!isLoading}>
+            <p className="text-md font-semibold bg-gray-200 h-4 w-1/3"></p>
           </Skeleton>
         </CardFooter>
       </Card>
@@ -129,7 +143,7 @@ function CarCard({ car, isLoading }: CardProps) {
   return (
     <>
       <Card
-        className="w-full md:max-w-[400px] shadow-lg"
+        className="md:w-lg md:h-lg w-sm shadow-2xl"
         radius="sm"
         isPressable={true}
         isHoverable={true}
