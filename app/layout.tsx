91b6f68765736bb5455799b55a13ad5d63bbc526
Rafael <rafaelgorayb388@gmail.com>
2024-08-01 10:16:33 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning className="overflow-x-hidden" lang="pt">
+    <html suppressHydrationWarning className="overflow-x-hidden bg-background" lang="pt">
       <head />
       <body
         className={clsx(
