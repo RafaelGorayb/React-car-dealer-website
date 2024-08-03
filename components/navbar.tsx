a43f3a0e5283a55c22c5789bf87d145a132d53bf
hhs0001 <heitorstein@gmail.com>
2024-08-03 19:21:55 -0300

    update 0.7
    - barra de pesquisa otimizada e bur de sobreposição corrigido

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -79,12 +79,12 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        {searchInput}
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
 
       <NavbarMenu>
+        <SearchBar isExpanded={true} />
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
